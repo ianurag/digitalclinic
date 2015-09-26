7717f64724ab61799df8ef10b21ded52c7e3c5d4
ianurag <anuragchatterjee@anurags-MacBook-Pro.local>
2015-09-26 09:35:59 +0530

    patient

--- doctor/assets/js/scripts.js
+++ doctor/assets/js/scripts.js
@@ -3,6 +3,58 @@
         // Replace onSearchSubmit() and onKeyEnter() with 
         // your logic to perform a search and display results
 
+    $('#patient-dob').datepicker();
+    $('#patient-member-dob').datepicker();
+
+    $('#datepicker-range').datepicker();
+
+    $("#phone").mask("999-999-9999");
+    $("#creditcard").mask("9999-9999-9999-9999-9999")
+   
+
+
+
+     var table = $('#myDataTable,#doc-history');
+
+var settings = {
+    "sDom": "<'table-responsive't><'row'<p i>>",
+    
+    "destroy": true,
+    "scrollCollapse": true,
+    "oLanguage": {
+        "sLengthMenu": "_MENU_ ",
+        "sInfo": "Showing <b>_START_ to _END_</b> of _TOTAL_ entries"
+    },
+    "iDisplayLength": 5
+};
+
+table.dataTable(settings);
+
+$('#submitAppointment').click(function(event) {
+    
+    $('#myModal').hide();
+    $('#modalappointmentpurpose').modal()
+});
+
+$('#submitappointmentdetail').click(function(event) {
+    
+    $('#modalappointmentpurpose').hide();
+    $('#modalappointmentpayment').modal()
+});
+
+$('#submitappointmentpayment').click(function(event) {
+    
+    $('#modalappointmentpayment').hide();
+    $('#apointmentshare').modal()
+
+});
+
+$('#finishappointment').click(function(event) {
+    $('.modal').modal('hide');
+
+    // $('.modal-backdrop').hide()
+});
+
 
         var elems = Array.prototype.slice.call(document.querySelectorAll('.switchery'));
 // Success color: #10CFBD
